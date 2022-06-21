59578730d4b12e25df404ccfbf7b3f2760f077e8
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-06-21 12:43:27 +0530

    implement push notification component

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -5,10 +5,14 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
+import { Textarea } from "@chakra-ui/react";
+
+import c from "./RightSide.module.css";
+import { Button } from "@mui/material";
 
 const RightSide = () => {
   return (
-    <Stack>
+    <Stack spacing={2}>
       <Paper margin="10px">
         <Grid
           direction="column"
@@ -25,6 +29,33 @@ const RightSide = () => {
             mb={"20px"}
           />
           <Typography variant="h5">Nasyx Nadeem</Typography>
+        </Grid>
+      </Paper>
+      <Paper>
+        <Grid
+          direction="column"
+          container
+          alignItems="center"
+          justifyContent="center"
+          height="300px"
+          // paddingLeft="30px"
+          sx={{ width: "100%" }}
+          spacing={4}
+        >
+          <Grid item>
+            <Typography variant="h6">Send push notifications</Typography>
+          </Grid>
+          <Grid item>
+            <Textarea
+              placeholder="Send notification to all users"
+              className={c.textfield}
+            />
+          </Grid>
+          <Grid item>
+            <Button variant="contained" size="small">
+              Send Notification
+            </Button>
+          </Grid>
         </Grid>
       </Paper>
     </Stack>
--- src/components/analytics/analyticsContent.js
+++ src/components/analytics/analyticsContent.js
@@ -636,12 +636,5 @@ export const optionsData = {
       backgroundColor: "rgba(255, 99, 132, 0.5)",
       yAxisID: "y",
     },
-    {
-      label: "Users",
-      data: labels.map(() => [100, 200, 300, 150, 200, 500, 700]),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-      yAxisID: "y1",
-    },
   ],
 };
